1ee7e1e40a4fd43eed3c0f6368a8446ab295d512
srojgam0907 <srojgam0907@g.educaand.es>
2026-02-13 14:15:14 +0100

    Enum de tipo de operacion

--- src/dominio/TipoOperador.java
+++ src/dominio/TipoOperador.java
@@ -2,4 +2,20 @@ package dominio;
 
 public enum TipoOperador {
 
+	suma('+'),
+	resta('-'),
+	multiplicacion('*'),
+	division('/');
+
+	private char symbol;
+	
+	TipoOperador(char symbol) {
+		this.symbol= symbol;
+	}
+
+
+    public char getSymbol() {
+        return symbol;
+    }
+
 }
