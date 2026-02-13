e9f14659476d7713de4898bea75409dd1fe32a50
srojgam0907 <srojgam0907@g.educaand.es>
2026-02-13 14:38:53 +0100

    Clase consola sin terminar

--- src/app/Consola.java
+++ src/app/Consola.java
@@ -1,5 +1,18 @@
 package app;
 
+import java.util.Scanner;
+
 public class Consola {
 
+	@SuppressWarnings("unused")
+	private Scanner kb= new Scanner(System.in);
+	
+	/**
+     * Muestra un texto por consola sin salto de línea.
+     *
+     * @param texto Texto que se desea mostrar.
+     */
+    public void escribir(String texto) { //Hay que hacer el bucle que comprube que está bien escrito
+        System.out.print(texto);
+    }
 }
