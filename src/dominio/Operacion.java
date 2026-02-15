aa62685d02c08868330d6e26831a79af8565ba0f
srojgam0907 <srojgam0907@g.educaand.es>
2026-02-15 16:28:24 +0100

    Metodos de la clase Operacion para devolver las operaciones del usuario

--- src/dominio/Operacion.java
+++ src/dominio/Operacion.java
@@ -2,4 +2,30 @@ package dominio;
 
 public class Operacion {
 
+	private final String entrada;
+	private final double resultado;
+	
+	/**
+	 * Crea un nuevo objeto Operacion
+	 * @param entrada La operacion que introduce el usuario
+	 * @param resultado El resultado calculado
+	 */
+	public Operacion(String entrada, double resultado) {
+		this.entrada= entrada;
+		this.resultado= resultado;
+	}
+	
+	public String getEntrada() {
+		return entrada;
+	}
+	
+	public double getResultado() {
+		return resultado;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s = %.2f", entrada, resultado);
+	}
+
 }
