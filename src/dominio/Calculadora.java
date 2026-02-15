1b3e11a8a1dffb899594ec3f6f10c891d4179eb2
srojgam0907 <srojgam0907@g.educaand.es>
2026-02-15 17:20:39 +0100

    Metodos para las cuatro operaciones básicas

--- src/dominio/Calculadora.java
+++ src/dominio/Calculadora.java
@@ -1,5 +1,34 @@
 package dominio;
 
+import java.util.ArrayList;
+import java.util.List;
+import app.Consola;
+
 public class Calculadora {
+
+	private double resultadoFinal;
+	private List<Operacion> historial;
+	
+	public Calculadora() {
+		this.resultadoFinal= 0;
+		this.historial= new ArrayList<>();
+	}
+	
+	private double sumar(double a, double b) {
+		return a + b;
+	}
+	
+	private double restar(double a, double b) {
+		return a - b;
+	}
+	
+	private double multiplicar(double a, double b) {
+		return a * b;
+	}
+	
+	private double dividir(double a, double b) {
+		return a / b; 
+
+	}
 
 }
