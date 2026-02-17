083f51144d66da50aa3d5e229f7d45eab49aabb0
srojgam0907 <srojgam0907@g.educaand.es>
2026-02-17 17:59:33 +0100

    Documentacion de la clase operacion

--- src/dominio/Operacion.java
+++ src/dominio/Operacion.java
@@ -15,14 +15,26 @@ public class Operacion {
 		this.resultado= resultado;
 	}
 	
+	/**
+	 * Devuelve la entrada por teclado original
+	 * @return La entrada original del usuario
+	 */
 	public String getEntrada() {
 		return entrada;
 	}
 	
+	/**
+	 * Devuelve el resultado de la operacion introducida por el usuario
+	 * @return Resultado obtenido de la operación
+	 */
 	public double getResultado() {
 		return resultado;
 	}
 
+	/**
+	 * Formatea la forma en la se muestra el historial por pantalla
+	 * @return La cadena con la operacion y el resultado
+	 */
 	@Override
 	public String toString() {
 		return String.format("%s = %.2f", entrada, resultado);
