05d45feae19fe85cab87570469390db419366359
srojgam0907 <srojgam0907@g.educaand.es>
2026-02-17 18:15:36 +0100

    Documentacion de la clase Calculadora

--- src/dominio/Calculadora.java
+++ src/dominio/Calculadora.java
@@ -8,28 +8,63 @@ public class Calculadora {
 	private double resultadoFinal;
 	private List<Operacion> historial;
 	
+	/**
+	 * Crea un nuevo objeto Calculadora
+	 */
 	public Calculadora() {
 		this.resultadoFinal= 0;
 		this.historial= new ArrayList<>();
 	}
 	
+	/**
+	 * Realiza la operación sumar
+	 * @param a
+	 * @param b
+	 * @return resultado de la suma de los parametros
+	 */
 	private double sumar(double a, double b) {
 		return a + b;
 	}
 	
+	/**
+	 * Realiza la operación restar
+	 * @param a
+	 * @param b
+	 * @return resultado de la resta de los parametros
+	 */
 	private double restar(double a, double b) {
 		return a - b;
 	}
 	
+	/**
+	 * Realiza la operación multiplicar
+	 * @param a
+	 * @param b
+	 * @return resultado de la multiplicación de los parametros
+	 */
 	private double multiplicar(double a, double b) {
 		return a * b;
 	}
 	
+	/**
+	 * Realiza la operación dividir
+	 * @param a
+	 * @param b
+	 * @return resultado de la división de los parametros
+	 */
 	private double dividir(double a, double b) {
 		return a / b; 
 
 	}
 	
+	/**
+	 * Realiza una serie de operaciones de izquierda a derecha. 
+	 * No hay prioridad de operadores
+	 * @param numeros Lista de los numeros de la operación
+	 * @param operadores Lista de los operadores que se usarán
+	 * @param entrada La operacion que escribio el usuario
+	 * @return El resultado final de la operación
+	 */
 	public double calcular(List<Double> numeros, List<TipoOperador> operadores, String entrada) {
 		double resultadoAcumulado= numeros.get(0);
 		
@@ -44,6 +79,14 @@ public class Calculadora {
 		return resultadoAcumulado;
 	}
 
+	/**
+	 * Selecciona, segun el operador pasado por parametro, 
+	 * a que metodo llamar para realizar la operacion correspondiente 
+	 * @param a
+	 * @param operador
+	 * @param b
+	 * @return el resultado de la operacion correspondiente
+	 */
 	private double ejecutar(double a, TipoOperador operador, double b) {
 
 		return switch(operador) {
@@ -55,15 +98,27 @@ public class Calculadora {
 		
 	}
 	
+	/**
+	 * Reinicia la calculadora. Pone el ultimo resultado 
+	 * guardado a 0 y vacía el historial
+	 */
 	public void resetear() {
 		this.resultadoFinal= 0;
 		this.historial.clear();
 	}
 	
+	/**
+	 * Devuelve el ultimo resultado guardado
+	 * @return Ultimo resultado guardado
+	 */
 	public double getResultadoFinal() {
 		return resultadoFinal;
 	}
 	
+	/**
+	 * Devuelve el historial de operaciones realizadas 
+	 * @return lista de operaciones
+	 */
 	public List<Operacion> getHistorial() {
 		return new ArrayList<>(historial); 
 	}
